77fca4475afb2c6e425ad2b13f516199b116e85a
MateuszKuba <mateusz.kuba@onet.eu>
2017-07-18 20:50:38 +0200

    POST requests

--- headlines.py
+++ headlines.py
@@ -18,9 +18,9 @@ RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
 
 
 
-@app.route("/")
+@app.route("/", methods=['GET','POST'])
 def get_news(number = "bbc"):
-    query = request.args.get("publication")
+    query = request.form.get("publication")
     if not query or query.lower() not in RSS_FEEDS:
         publication = "bbc"
     else:
